ca6ddb3a9feec075c3f9ec0e9d2005776c8a24b2
Michael Rosenberger <leahcim165@gmail.com>
2013-07-15 15:59:44 -0700

    Working demo with simple strings going to b.

--- framework.py
+++ framework.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import random
 
 class Individual:
@@ -18,9 +20,11 @@ class Genome:
         self.content = content
 
     def get_mutation(self):
-        char_index = random.randint(2, len(self.content) - 2)
-        return (self.content[:char_index - 1] + 
-                random.choice('abcdefghijklmnopqrstuvwxyz') + self.content[char_index:])
+        char_index = random.randint(0, len(self.content) - 1)
+        char = random.choice('abcdefghijklmnopqrstuvwxyz')
+        listed = list(self.content)
+        listed[char_index] = char
+        return ''.join(map(str, listed))
 
 class Environment:
 
@@ -28,34 +32,54 @@ class Environment:
         pass
 
     def score(self, individual):
-        return len(filter(lambda c:c=='a', individual.genome.content))
+        return len(filter(lambda c:c=='b', individual.genome.content))
 
 class Runner:
 
     def generate_children(self, individual, num):
         results = []
         for i in range(num):
-            results.append(Individual(individual.genome.get_mutation()))
+            results.append(Individual(Genome(individual.genome.get_mutation())))
         return results
 
     def tick(self): # Sort by score, choose best, mutate, score mutations
         self.generations[-1].sort(key=lambda i:i.get_score())
         best = self.generations[-1][-self.skim_depth:]
-        children = map(lambda i:generate_children(i, self.num_children))
+        children = map(lambda i:self.generate_children(i, self.num_children), best)
         self.generations.append(sum(children, []))
         for child in self.generations[-1]:
             child.set_score(self.environment.score(child))
-        
-    def __init__(self, seed_individuals):
-        self.environment = Environment()
-        self.generation_size = 20 # Size of each generation
-        self.skim_depth = 5 # How many of the top individuals we mutate from
-        # Ensure that skim_depth is an even factor of generation_size
-        self.num_children = self.generation_size / self.skim_depth
-        self.generations = []
-        self.generations.append(seed_individuals)
 
     def run(self, iterations):
         for i in range(iterations):
             self.tick()
-        
+
+    def generate_initial_individuals(self, num):
+        individuals = []
+        for i in range(num):
+            genome = ''
+            for j in range(10):
+                genome += random.choice('abcdefghijklmnopqrstuvwxy')
+            individuals.append(Individual(Genome(genome)))
+        return individuals
+
+    def __init__(self, generation_size=20, skim_depth=5, seed_individuals=None):
+        self.environment = Environment()
+        self.generation_size = generation_size # Size of each generation
+        self.skim_depth = skim_depth # How many of the top individuals we mutate from (must be factor of generation_size)
+        self.num_children = self.generation_size / self.skim_depth
+        self.generations = []
+        if seed_individuals:
+            self.generations.append(seed_individuals)
+        else:
+            self.generations.append(self.generate_initial_individuals(self.generation_size))
+
+r = Runner()
+while (True):
+    print('Individuals:')
+    for individual in r.generations[-1]:
+        print(individual.genome.content)
+    raw_input()
+    print('Ticking...')
+    r.tick()
+    print('Done')
